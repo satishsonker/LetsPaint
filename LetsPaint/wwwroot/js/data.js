e06c4e51d566ba5de926a587a7e3475a94806f52
Satish Sonkar <satish.sonkar@nagarro.com>
2020-05-29 11:43:20 +0530

    New Updated

---
+++ LetsPaint/wwwroot/js/data.js
@@ -0,0 +1,37 @@
+﻿const galleries = [
+    { id: 1, name: 'Painting', baseUrl: "/images/Product/Painting/", gridSize: 4 },
+    { id: 2, name: 'Craft', baseUrl: "/images/Product/Craft/", gridSize: 4 },
+    { id: 3, name: 'Rangoli', baseUrl: "/images/Product/Rangoli/", gridSize: 4 },
+    { id: 4, name: 'Wall Painting', baseUrl: "/images/Product/WallPainting/", gridSize: 4 }
+];
+const galleryData = [
+    { id: 1, galleryId: 1, title: "Madhubani Art", thumbnail: "14_sm.jpg", image: "14.jpeg", badge: "New",artist:"Poonam Sonkar"},
+    { id: 2, galleryId: 1, title: "African Women", thumbnail: "1_sm.jpg", image: "1.jpeg", badge: "New",artist:"Poonam Sonkar" },
+    { id: 3, galleryId: 1, title: "Birds", thumbnail: "2_sm.jpg", image: "2.jpg", badge: "New",artist:"Poonam Sonkar" },
+    { id: 4, galleryId: 1, title: "Lord Ganesha", thumbnail: "3_sm.jpg", image: "3.jpg", badge: "New",artist:"Poonam Sonkar" },
+    { id: 5, galleryId: 1, title: "Tribal Art/African", thumbnail: "4_sm.jpg", image: "4.jpg", badge: "New",artist:"Poonam Sonkar" },
+    { id: 6, galleryId: 1, title: "Mother's special", thumbnail: "5_sm.jpg", image: "5.jpg", badge: "New",artist:"Poonam Sonkar" },
+    { id: 7, galleryId: 1, title: "Lord Buddha", thumbnail: "6_sm.jpg", image: "6.jpg", badge: "New",artist:"Poonam Sonkar" },
+    { id: 8, galleryId: 1, title: "Then & Now", thumbnail: "7_sm.jpg", image: "7.jpg", badge: "New",artist:"Poonam Sonkar" },
+    { id:9, galleryId: 1, title: "Valentine's Special", thumbnail: "8_sm.jpg", image: "8.jpg", badge: "New",artist:"Poonam Sonkar" },
+    { id:10, galleryId: 1, title: "Love in Air", thumbnail: "9_sm.jpg", image: "9.jpg", badge: "New",artist:"Poonam Sonkar" },
+    { id: 11, galleryId: 1, title: "Wildlife", thumbnail: "10_sm.jpg", image: "10.jpg", badge: "New",artist:"Poonam Sonkar" },
+    { id: 12, galleryId: 1, title: "Immortal Hanuman", thumbnail: "11_sm.jpg", image: "11.jpg", badge: "New",artist:"Poonam Sonkar" },
+    { id: 12, galleryId: 1, title: "Floral Design", thumbnail: "12_sm.jpeg", image: "12.jpeg", badge: "New", artist: "Poonam Sonkar" },
+
+    { id: 25, galleryId: 2, title: "Photo Frame", thumbnail: "5_sm.jpg", image: "5.jpg", badge: "New", artist: "Poonam Sonkar" },
+    { id: 13, galleryId: 2, title: "Egg Cell Smiley", thumbnail: "4_sm.jpg", image: "4.jpeg", badge: "New",artist:"Poonam Sonkar" },
+    { id: 14, galleryId: 2, title: "Wildlife", thumbnail: "1_sm.jpg", image: "1.jpg", badge: "New",artist:"Poonam Sonkar" },
+    { id: 15, galleryId: 2, title: "Immortal Hanuman", thumbnail: "2_sm.jpg", image: "2.jpg", badge: "New",artist:"Poonam Sonkar" },
+    { id: 16, galleryId: 2, title: "Floral Design", thumbnail: "3_sm.jpg", image: "3.jpg", badge: "New",artist:"Poonam Sonkar" },
+
+    { id: 17, galleryId: 3, title: "Immortal Hanuman", thumbnail: "1_sm.jpg", image: "1.jpg", badge: "New",artist:"Poonam Sonkar" },
+    { id: 18, galleryId: 3, title: "Floral Design", thumbnail: "2_sm.jpg", image: "2.jpg", badge: "New",artist:"Poonam Sonkar" },
+    { id: 19, galleryId: 3, title: "Love in Air", thumbnail: "3_sm.jpg", image: "3.jpeg", badge: "New",artist:"Poonam Sonkar" },
+    { id: 20, galleryId: 3, title: "Wildlife", thumbnail: "4_sm.jpg", image: "4.jpg", badge: "New",artist:"Poonam Sonkar" },
+    { id: 21, galleryId: 3, title: "Immortal Hanuman", thumbnail: "5_sm.jpg", image: "5.jpg", badge: "New",artist:"Poonam Sonkar" },
+    { id: 22, galleryId: 3, title: "Floral Design", thumbnail: "6_sm.jpg", image: "6.jpg", badge: "New",artist:"Poonam Sonkar" },
+
+    { id: 23, galleryId: 4, title: "Immortal Hanuman", thumbnail: "1_sm.jpg", image: "1.jpeg", badge: "New",artist:"Poonam Sonkar" },
+    { id: 24, galleryId: 4, title: "Floral Design", thumbnail: "2_sm.jpg", image: "2.jpeg", badge: "New",artist:"Poonam Sonkar" },
+];
---
+++ LetsPaint/wwwroot/js/gallery.js
@@ -0,0 +1,55 @@
+﻿$(document).ready(function () {
+    var $galleriesContainer = $('.galleries ul'), $galleriesList = '', $query = getUrlVars(), $galId = parseInt($query.galId);
+    $($galleriesContainer).empty();
+    $(galleries).each(function (ind, ele) {
+        $galleriesList += `<li data-id="${ele.id}" class="${ele.id === $galId ? 'active' : ''}" data-baseurl="${ele.baseUrl}">${ele.name}</li>`;
+    });
+    $($galleriesContainer).append($galleriesList);
+    bindGallery($galId === undefined || isNaN($galId) ? galleries[0].id : $galId);
+    if (isNaN($galId)) {
+        $('.galleries ul li').eq(0).addClass('active');
+    }
+
+});
+$(document).on('click', '.galleries ul li', function () {
+    var $id = parseInt($(this).data('id'));
+    bindGallery($id);
+    $('.galleries ul li').removeClass('active');
+    $(this).addClass('active');
+});
+
+function bindGallery($galId) {
+    var $galData = galleries.filter(x => x.id === $galId);
+    var $container = $('.product-section .container');
+    $($container).find('.row:gt(0)').remove();
+    var $list = ``;
+    $(galleryData.filter(x => x.galleryId === $galData[0].id)).each(function (ind, ele) {
+        if (ind === 0) {
+            $list += '<div class="row">';
+        } if (ind % $galData[0].gridSize === 0 && ind !== 0) {
+            $list += '</div><div class="row">';
+        }
+        $list += `<div class="col-md">
+                <a class="gallery-item" href="${$galData[0].baseUrl}${ele.image}" data-lightbox="mygallery">
+                    <div class="gallery-container">
+                        <div class="overlay-artist">Artist - ${ele.artist}</div>
+                        <img src="${$galData[0].baseUrl}${ele.thumbnail}" class="image" />
+                        <div class="overlay">${ele.title}</div>
+                    </div>
+                </a>
+            </div>`;
+
+    });
+    $($container).append($list + '</div>');
+}
+
+function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
